56338b064cf6061b9e6103d8cd1b1f69d9d3344d
Pallavi Soldey <pallavisolday12@gmail.com>
2023-10-03 12:11:51 +0530

    change origin cors

--- controllers/SendMailController.js
+++ controllers/SendMailController.js
@@ -35,7 +35,7 @@ const SendEmails = async(req , res) =>{
     }
     catch(error){
         console.log(error)
-        return res.status(500).json({error : "Internal Error"})
+        return res.status(500).json({error : error.message});
     }
 }
 
